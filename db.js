a505a346a54c762fe3cc0921cb80179313f7b114
olrlvl <tlsgutjq94@naver.com>
2024-02-28 21:08:39 +0900

    🎨 error handling in database connection

--- db.js
+++ db.js
@@ -15,8 +15,28 @@ const pool = new Pool({
 });
 
 // 데이터베이스 연결 상태
-pool.connect((err) =>
-    err ? console.error("데이터베이스 연결 에러", err.stack) : console.log("데이터베이스 연결 성공")
-);
+pool.connect((err) => {
+    if (err) {
+        console.error("데이터베이스 연결 에러", err.stack);
+        console.log("에러 메시지:", err.message);
+        console.log("에러 코드:", err.code);
+        console.log("세부 정보:", err.detail);
+        console.log("힌트:", err.hint);
+        console.log("위치:", err.position);
+        console.log("내부 위치:", err.internalPosition);
+        console.log("내부 쿼리:", err.internalQuery);
+        console.log("어디에서:", err.where);
+        console.log("스키마:", err.schema);
+        console.log("테이블:", err.table);
+        console.log("컬럼:", err.column);
+        console.log("데이터 타입:", err.dataType);
+        console.log("제약 조건:", err.constraint);
+        console.log("파일:", err.file);
+        console.log("라인:", err.line);
+        console.log("루틴:", err.routine);
+    } else {
+        console.log("데이터베이스 연결 성공");
+    }
+});
 
 export default pool;
